2e4bddf06031e74f3798902b0c952c4d3bab991b
mccrush <mccrush2027@gmail.com>
2018-09-23 22:24:50 +0700

    Дорабатывает расчет суммы работы

--- src/components/Rabota.vue
+++ src/components/Rabota.vue
@@ -1,8 +1,8 @@
 <template>
   <tr>
-    <td class="text-left pl-2">{{ rabotaFrom.title }}</td>
+    <td class="text-left pl-2" vertical-align="baseline">{{ rabotaFrom.title }}</td>
     <td>{{ rabotaFrom.price }}</td>
-    <td width="10%"><input v-if="rabotaFrom.id !== 1 && rabotaFrom.id !== 2" class="form-control form-control-sm" type="number" placeholder="1" step="1" min="0" max="99" v-on:input="comput" v-model.number="counter"></td>
+    <td width="10%"><input v-if="rabotaFrom.id !== 1 && rabotaFrom.id !== 2" class="form-control form-control-sm" type="number" placeholder="1" step="1" min="1" max="99" v-on:input="comput" v-model.number="counter"></td>
     <td>
       <div class="btn-group btn-group-sm" role="group" aria-label="Basic example">
         <button type="button" class="btn btn-secondary active">Нет</button>
@@ -32,7 +32,25 @@ export default {
   },
   methods: {
     comput: function() {
-      this.summa = this.counter * this.price - 50 * (this.counter - 1);
+      let summa =
+        this.counter * this.price - this.price / 4 * (this.counter - 1);
+      //console.log('summa = ', summa);
+
+      summa = Math.ceil(summa);
+      //console.log('okrugl summa = ', summa);
+
+      let ostatok = summa % 10;
+      //console.log('ostatok na 10 = ', ostatok);
+
+      summa = summa - ostatok;
+      //console.log('summa bez ostatka = ', summa);
+
+      ostatok = ostatok > 5 ? 10 : 0;
+      //console.log('noviy ostatok = ', ostatok);
+
+      summa = summa + ostatok;
+      //console.log('novaya summa = ', summa);
+      this.summa = summa;
     }
   }
 };
@@ -40,4 +58,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.table td,
+.table th {
+  vertical-align: middle;
+}
 </style>
