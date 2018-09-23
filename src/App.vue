748a3c5ed2199f3f80e589501dbd534acd105c69
mccrush <mccrush2027@gmail.com>
2018-09-23 13:36:41 +0700

    Создает новые компоненты

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <Header msg="Welcome to Header"/>
+    <Hellow msg="Welcome to Hellow"/>
+    <Calc msg="Welcome to Calc"/>
+    <Footer msg="Welcome to Footer"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue';
+import Hellow from './components/Hellow.vue';
+import Calc from './components/Calc.vue';
+import Footer from './components/Footer.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header,
+    Hellow,
+    Calc,
+    Footer
   }
-}
+};
 </script>
 
 <style>
@@ -23,6 +32,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-whit shadow-sm">
+      <img class="my-0 mr-md-auto pr-5" alt="Vue logo" src="../assets/logo.png" height="24">
+      <h5 class="my-0 ml-md-auto font-weight-normal">8 906 940 4069</h5>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
