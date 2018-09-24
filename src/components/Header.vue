5dc599d06e85950627ebdbf2ca80b99c1bf661cb
mccrush <mccrush2027@gmail.com>
2018-09-24 08:39:39 +0700

    Настраивает Хэдер

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,9 @@
 <template>
-  <div id="header">
+  <div id="header shadow-sm">
 
-    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 bg-whit shadow-sm">
+    <div class="d-flex flex-column pricing-header flex-md-row  mx-auto align-items-center p-3 px-md-4 bg-whit">
       <img class="my-0 mr-md-auto" alt="Vue logo" src="../assets/logo.png" height="24">
-      <h5 class="my-0 ml-md-auto font-weight-normal">8 906 940 4069</h5>
+      <h5 class="my-0 ml-md-auto lead">8 906 940 4069</h5>
     </div>
     
   </div>
@@ -20,4 +20,11 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.material-icons {
+  font-size: 18px;
+}
+
+.pricing-header {
+  max-width: 700px;
+}
 </style>
