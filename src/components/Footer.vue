47bed1a633b914f8c8f43b9c8abc3e1257b6d001
mccrush <mccrush2027@gmail.com>
2020-11-14 17:31:23 +0700

    Update footer info

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,44 +1,77 @@
 <template>
   <div id="footer">
-    
-    <div class="container">  
+    <div class="container">
       <footer class="pt-4 mx-auto">
         <div class="row">
-           <div class="col-12 col-sm-5 border-right text-left">
+          <div class="col-12 col-sm-5 border-right text-left">
             <!-- <img class="mb-2" src="../../assets/brand/bootstrap-solid.svg" alt="" width="24" height="24"> -->
-            <small class="d-block mb-3">* Предложение не является публичной офертой!<br><br>
-            ** Каждая следующая <abbr title="Замена розетки, выключателя и т.д.">точка</abbr> на 25% дешевле суммы предыдущих.
+            <small class="d-block mb-3"
+              >* Предложение не является публичной офертой!<br /><br />
+              ** Каждая следующая
+              <abbr title="Замена розетки, выключателя и т.д.">точка</abbr> на
+              25% дешевле суммы предыдущих.
             </small>
           </div>
           <div class="col-6 col-sm-3 border-right text-left">
             <h6>Режим работы</h6>
             <ul class="list-unstyled">
-              <li><small>Вт: 9:00 - 18:00</small></li>
-              <li><small>Ср: 9:00 - 18:00</small></li>
-              <li><small>Чт: 9:00 - 18:00</small></li>
-              <li><small>Пт: 9:00 - 18:00</small></li>
-              <li><small>Сб: 10:00 - 15:00</small></li>
+              <li><small>Вт: 10:00 - 20:00</small></li>
+              <li><small>Ср: 10:00 - 20:00</small></li>
+              <li><small>Чт: 10:00 - 20:00</small></li>
+              <li><small>Пт: 10:00 - 20:00</small></li>
             </ul>
           </div>
           <div class="col-6 col-sm-4 text-left">
             <h6>Контакты</h6>
             <ul class="list-unstyled">
-              <li class="d-flex align-items-center"><i class="material-icons">phone</i><a class="text-white-50 ml-2" href="tel:+79069404069">8 906 940 4069</a></li>
-              <li class="d-flex align-items-center"><i class="material-icons">language</i> <a class="text-white-50 ml-2" href="http://em5electric.ru" target="_self">em5electric.ru</a></li>
-              <li class="d-flex align-items-center"><i class="material-icons">open_in_new</i> <a class="text-white-50 ml-2" href="http://vk.com/em5electric" target="_blank">vk.com/em5electric</a></li>
-              <li class="d-flex align-items-center"><i class="material-icons">mail_outline</i><a class="text-white-50 ml-2" href="mailto:support@em5electric.ru?subject=Вопрос электрику"><small>support@em5electric.ru</small></a></li>
-              <li class="d-flex align-items-center"><i class="material-icons">location_on</i><span class="text-white-50 ml-2">г. Славгород</span></li>
+              <li class="d-flex align-items-center">
+                WA
+                <a class="text-white-50 ml-2" href="tel:+79069404069"
+                  >8 906 940-40-69</a
+                >
+              </li>
+              <li class="d-flex align-items-center">
+                <i class="material-icons">language</i>
+                <a
+                  class="text-white-50 ml-2"
+                  href="https://mccrush.github.io/em5/"
+                  target="_self"
+                  >mccrush.github.io/em5</a
+                >
+              </li>
+              <li class="d-flex align-items-center">
+                <i class="material-icons">open_in_new</i>
+                <a
+                  class="text-white-50 ml-2"
+                  href="http://vk.com/em5electric"
+                  target="_blank"
+                  >vk.com/em5electric</a
+                >
+              </li>
+              <!-- <li class="d-flex align-items-center">
+                <i class="material-icons">mail_outline</i
+                ><a
+                  class="text-white-50 ml-2"
+                  href="mailto:support@em5electric.ru?subject=Вопрос электрику"
+                  ><small>support@em5electric.ru</small></a
+                >
+              </li> -->
+              <li class="d-flex align-items-center">
+                <i class="material-icons">location_on</i
+                ><span class="text-white-50 ml-2">г. Славгород</span>
+              </li>
             </ul>
           </div>
         </div>
         <div class="row">
           <div class="col-12">
-            <small class="d-block mb-3 mt-4">2018 © «ЭМ5 электрик»</small>
+            <small class="d-block mb-3 mt-4"
+              >2014 - 2020 © «ЭМ5 электрик»</small
+            >
           </div>
         </div>
       </footer>
     </div>
-
   </div>
 </template>
 
@@ -46,9 +79,9 @@
 export default {
   name: 'Footer',
   props: {
-    msg: String
-  }
-};
+    msg: String,
+  },
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
