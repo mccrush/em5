94d1e7959242d7d6404a96c7dbbbdce21ab6a951
mccrush <mccrush2027@gmail.com>
2020-11-14 17:25:59 +0700

    Update calc methods

--- src/components/Calc.vue
+++ src/components/Calc.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="calc" class="pt-3 pb-3">
-
     <div class="container">
-      <table class="table table-striped table-borderless table-sm table-responsive-sm">
+      <table
+        class="table table-striped table-borderless table-sm table-responsive-sm"
+      >
         <thead>
           <tr>
             <th scope="col">Вид работ*</th>
@@ -13,7 +14,6 @@
           </tr>
         </thead>
         <tbody>
-
           <tr>
             <td class="text-left pl-2" vertical-align="baseline">
               <span class="d-none d-sm-block">Вызов электрика</span>
@@ -22,83 +22,71 @@
             <td>50</td>
             <td width="10%"></td>
             <td>
-              <div class="btn-group btn-group-sm" role="group" aria-label="Basic example">
-                <button type="button" class="btn btn-light disabled">Нет</button>
-                <button type="button" class="btn btn-success active disabled">Да&nbsp;</button>
+              <div
+                class="btn-group btn-group-sm"
+                role="group"
+                aria-label="Basic example"
+              >
+                <button type="button" class="btn btn-light disabled">
+                  Нет
+                </button>
+                <button type="button" class="btn btn-success active disabled">
+                  Да&nbsp;
+                </button>
               </div>
             </td>
             <td>50</td>
           </tr>
 
-          <Rabota v-for="rabota in listRabot" :key="rabota.id" :rabotaFrom="rabota" />
+          <Rabota
+            v-for="rabota in listRabot"
+            :key="rabota.id"
+            :rabotaFrom="rabota"
+            @add-summ="addSumm"
+            @remove-summ="removeSumm"
+          />
           <tr>
             <td colspan="4" class="text-right"><strong>Итого:</strong></td>
             <td>{{ totalSumma }}</td>
           </tr>
         </tbody>
       </table>
-
     </div>
-
   </div>
 </template>
 
 <script>
-import { loadList } from '../core/raboty.js';
-import { eventEmitter } from '../main.js';
-import Rabota from './Rabota.vue';
+import { loadList } from '../core/raboty.js'
+import Rabota from './Rabota.vue'
 
 export default {
   name: 'Calc',
-  props: {
-    msg: String
-  },
   components: {
-    Rabota
+    Rabota,
   },
-  data: function() {
+  data: function () {
     return {
       listRabot: loadList(),
       arraySumm: [],
-      totalSumma: '50'
-    };
+      totalSumma: '50',
+    }
   },
-  created() {
-    eventEmitter.$on('add-summ', (summFrom, id) => {
-      console.log('summFrom = ' + summFrom, 'id = ', id);
-      this.arraySumm[id] = summFrom;
-      console.log(this.arraySumm);
-      let totSum = 50;
-      this.arraySumm.forEach(function(item) {
-        totSum = totSum + +item;
-        console.log('Itogo = ', totSum);
-        //this.totalSumm = this.totalSumm + item;
-      });
-      this.totalSumma = totSum;
-      console.log('totalSumma = ', this.totalSumma);
-      // document.getElementById('inpitLink').classList.remove('is-invalid');
-      // this.id = linkFrom.id;
-      // this.href = linkFrom.href;
-      // this.title = linkFrom.title;
-    });
-
-    eventEmitter.$on('remove-summ', (summFrom, id) => {
-      console.log('summFrom = ' + summFrom, 'id = ', id);
-      this.arraySumm[id] = 0;
-      console.log(this.arraySumm);
-
-      this.totalSumma = this.totalSumma - summFrom;
-      console.log('totalSumma = ', this.totalSumma);
-      // document.getElementById('inpitLink').classList.remove('is-invalid');
-      // this.id = linkFrom.id;
-      // this.href = linkFrom.href;
-      // this.title = linkFrom.title;
-    });
-  }
-};
+  methods: {
+    addSumm(summFrom, id) {
+      this.arraySumm[id] = summFrom
+      let totSum = 50
+      this.arraySumm.forEach(function (item) {
+        totSum = totSum + +item
+      })
+      this.totalSumma = totSum
+    },
+    removeSumm(summFrom, id) {
+      this.arraySumm[id] = 0
+      this.totalSumma = this.totalSumma - summFrom
+    },
+  },
+}
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .container {
   max-width: 700px; /* 960px; */
--- src/components/Rabota.vue
+++ src/components/Rabota.vue
@@ -5,11 +5,47 @@
       <small class="d-sm-none">{{ rabotaFrom.title }}</small>
     </td>
     <td>{{ rabotaFrom.price }}</td>
-    <td width="10%"><input v-if="rabotaFrom.id !== 2" class="form-control form-control-sm" type="number" placeholder="1" step="1" min="1" max="99" v-on:input="comput" v-model.number="counter"></td>
+    <td width="10%">
+      <input
+        v-if="rabotaFrom.id !== 2"
+        class="form-control form-control-sm"
+        type="number"
+        placeholder="1"
+        step="1"
+        min="1"
+        max="99"
+        v-on:input="comput"
+        v-model.number="counter"
+      />
+    </td>
     <td>
-      <div class="btn-group btn-group-sm" role="group" aria-label="Basic example">
-        <button type="button" class="btn btn-light" v-bind:class="[ isActive ? '' : 'active', isActive ? 'btn-light': 'btn-light' ]" v-on:click="abortRaboty">Нет</button>
-        <button type="button" class="btn" v-bind:class="[ isActive ? 'active' : '', isActive ? 'btn-success': 'btn-light' ]" v-on:click="selectRaboty">Да&nbsp;</button>
+      <div
+        class="btn-group btn-group-sm"
+        role="group"
+        aria-label="Basic example"
+      >
+        <button
+          type="button"
+          class="btn btn-light"
+          v-bind:class="[
+            isActive ? '' : 'active',
+            isActive ? 'btn-light' : 'btn-light',
+          ]"
+          v-on:click="abortRaboty"
+        >
+          Нет
+        </button>
+        <button
+          type="button"
+          class="btn"
+          v-bind:class="[
+            isActive ? 'active' : '',
+            isActive ? 'btn-success' : 'btn-light',
+          ]"
+          v-on:click="selectRaboty"
+        >
+          Да&nbsp;
+        </button>
       </div>
     </td>
     <td v-if="showSumm">{{ summa }}</td>
@@ -18,12 +54,10 @@
 </template>
 
 <script>
-import { eventEmitter } from '../main.js';
-
 export default {
   name: 'Rabota',
   props: {
-    rabotaFrom: Object
+    rabotaFrom: Object,
   },
   data() {
     return {
@@ -32,58 +66,51 @@ export default {
       price: this.rabotaFrom.price,
       isActive: this.rabotaFrom.active,
       showSumm: false,
-      abort: false
-    };
-  },
-  created() {
-    //console.log('in Beforcreated rabotaFrom.price;: ', rabotaFrom.price);
-    //this.price = rabotaFrom.price;
+      abort: false,
+    }
   },
   methods: {
-    comput: function() {
+    comput: function () {
       let summa =
-        this.counter * this.price - this.price / 4 * (this.counter - 1);
+        this.counter * this.price - (this.price / 4) * (this.counter - 1)
       //console.log('summa = ', summa);
 
-      summa = Math.ceil(summa);
+      summa = Math.ceil(summa)
       //console.log('okrugl summa = ', summa);
 
-      let ostatok = summa % 10;
+      let ostatok = summa % 10
       //console.log('ostatok na 10 = ', ostatok);
 
-      summa = summa - ostatok;
+      summa = summa - ostatok
       //console.log('summa bez ostatka = ', summa);
 
-      ostatok = ostatok > 5 ? 10 : 0;
+      ostatok = ostatok > 5 ? 10 : 0
       //console.log('noviy ostatok = ', ostatok);
 
-      summa = summa + ostatok;
+      summa = summa + ostatok
       //console.log('novaya summa = ', summa);
-      this.summa = summa;
+      this.summa = summa
 
-      if (this.isActive)
-        eventEmitter.$emit('add-summ', this.summa, this.rabotaFrom.id);
-      this.abort = true;
+      if (this.isActive) this.$emit('add-summ', this.summa, this.rabotaFrom.id)
+      this.abort = true
     },
-    selectRaboty: function() {
-      this.showSumm = true;
-      this.isActive = true;
-      eventEmitter.$emit('add-summ', this.summa, this.rabotaFrom.id);
-      this.abort = true;
+    selectRaboty: function () {
+      this.showSumm = true
+      this.isActive = true
+      this.$emit('add-summ', this.summa, this.rabotaFrom.id)
+      this.abort = true
     },
-    abortRaboty: function() {
-      this.showSumm = false;
-      this.isActive = false;
+    abortRaboty: function () {
+      this.showSumm = false
+      this.isActive = false
       if (this.abort) {
-        eventEmitter.$emit('remove-summ', this.summa, this.rabotaFrom.id);
-        this.abort = false;
+        this.$emit('remove-summ', this.summa, this.rabotaFrom.id)
+        this.abort = false
       }
-    }
-  }
-};
+    },
+  },
+}
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .table td,
 .table th {
