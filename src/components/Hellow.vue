9c098c430b2c30cde78f31e161307c96f704408f
mccrush <mccrush2027@gmail.com>
2020-11-14 17:50:21 +0700

    Update hello info

--- src/components/Hellow.vue
+++ src/components/Hellow.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="hello" class="">
-
-    <div class="pricing-header px-3 py-3 pt-4 mx-auto text-center">
+    <div class="pricing-header px-3 py-3 pt-5 pb-5 mx-auto text-center">
       <!-- <h1 class="display-4">«ЭМ5 электрик»</h1> -->
-      <img class="my-0 mr-md-auto mt-1 mb-3" alt="Vue logo" src="../assets/logo.png" height="150">
-      <p class="lead mb-0">Чиним и ремонтируем ⚡ г.&nbsp;Славгород<br>Мелкосрочный ремонт бытовых электроприборов</p>
+      <img
+        class="my-0 mr-md-auto mt-1 mb-3"
+        alt="Vue logo"
+        src="../assets/logo.png"
+        height="150"
+      />
+      <p class="lead mt-2 mb-0">
+        Чиним и ремонтируем ⚡ г.&nbsp;Славгород<br />Мелкосрочный ремонт
+        бытовых электроприборов
+      </p>
     </div>
-
   </div>
 </template>
 
@@ -14,9 +20,9 @@
 export default {
   name: 'Hellow',
   props: {
-    msg: String
-  }
-};
+    msg: String,
+  },
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
