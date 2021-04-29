0c7a45a96ae0cc4418d7a11c20a8359e630c0cea
mccrush <mccrush2027@gmail.com>
2021-04-29 11:15:47 +0700

    Settings view navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <div id="app">
-    <Header />
-    <Hellow />
-    <Calc />
-    <Footer />
+  <div id="container" class="container-fluid">
+    <div class="row">
+      <Header />
+    </div>
+    <div class="row"><Hellow /></div>
+    <div class="row"><Calc /></div>
+    <div class="row"><Footer /></div>
   </div>
 </template>
 
@@ -26,9 +28,14 @@ export default {
 </script>
 
 <style>
-#app {
-  text-align: center;
-  color: #2c3e50;
+#container {
+  color: rgba(31, 31, 33, 0.87);
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 400;
+}
+
+.container-640 {
+  max-width: 640px;
 }
 
 .btn:focus,
@@ -45,5 +52,17 @@ export default {
 .accordion-button:not(.collapsed) {
   color: #212529;
   background-color: #f8f9fa;
+}
+
+.cursor-def {
+  cursor: default;
+}
+
+.cursor-point {
+  cursor: pointer;
+}
+
+.fw-500 {
+  font-weight: 600;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,21 +1,34 @@
 <template>
-  <div id="header" class="shadow-sm">
-    <div
-      class="d-flex flex-column pricing-header flex-md-row mx-auto align-items-center p-3 px-md-4"
-    >
-      <h5 class="my-0">«ЭМ5 электрик»</h5>
-      <h5 class="my-0 ml-md-auto">WA: +7 906 940-40-69</h5>
-    </div>
+  <div class="col-12 p-0">
+    <nav class="navbar bg-light">
+      <div class="container-640 container-lg pt-1">
+        <div class="cursor-def d-sm-none navbar-brand text-center w-100 me-0">
+          «ЭМ5 электрик»
+        </div>
+
+        <div class="cursor-def d-none d-sm-block navbar-brand">
+          <img
+            src="/img/icons/icon_32.png"
+            width="32"
+            height="32"
+            alt="Logo ЭМ5 электрик"
+            class="mb-0 me-2"
+          />
+          <span class="fw-500">«ЭМ5 электрик»</span>
+        </div>
+
+        <div class="d-sm-none d-flex justify-content-center w-100 mb-1">
+          <div class="text-end me-2" title="Пишите нам в WhatsApp">
+            <span class="cursor-def fw-500">WA: 89069404069</span>
+          </div>
+        </div>
+
+        <div class="d-none d-sm-flex justify-content-between">
+          <div class="text-end pt-2 me-3" title="Пишите нам в WhatsApp">
+            <span class="cursor-def fw-500">WA: 89069404069</span>
+          </div>
+        </div>
+      </div>
+    </nav>
   </div>
 </template>
-
-<script>
-export default {
-  name: 'Header',
-}
-</script>
-<style scoped>
-.pricing-header {
-  max-width: 700px;
-}
-</style>
