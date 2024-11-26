debd650f6f8efd5743c9c0d8d99fc2ceb95a8dbb
mccrush <mccrush2027@gmail.com>
2024-11-26 08:55:35 +0700

    Bild proj

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  root: '.',
-  // base: '/em5/',
+  //root: '.',
+  base: '/em5/',
   build: {
     outDir: 'docs'
   }
